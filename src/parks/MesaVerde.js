69f69e48bfac24e9350113ab4d8c8c53ee7fc762
shalom-nganga <minnomacharia@gmail.com>
2023-11-07 14:06:28 +0300

    made changes to the MesaVerde.js  file

--- src/parks/MesaVerde.js
+++ src/parks/MesaVerde.js
@@ -1,7 +1,22 @@
-import React from "react";
+// import React from "react";
 
-function MesaVerde() {
-  return <h1>Mesa Verde National Park</h1>;
+// function MesaVerde() {
+//   return <h1>Mesa Verde National Park</h1>;
+// }
+
+// export default MesaVerde;
+
+
+
+import React from "react";
+import MesaVerde from "./parks/MesaVerde";
+
+function ColoradoStateParks() {
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  );
 }
 
-export default MesaVerde;
+export default ColoradoStateParks;
